d417389caf87e482a56a83facb41fa0ee0f61940
ARTurchinovich <turchinovichar@gmail.com>
2020-10-28 00:16:06 +0300

    Added Vet List and updated controller. Closes #15

--- pet-clinic-web/src/main/java/ru/turchinovich/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ru/turchinovich/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package ru.turchinovich.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.turchinovich.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetController  {
 
+    final private VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
