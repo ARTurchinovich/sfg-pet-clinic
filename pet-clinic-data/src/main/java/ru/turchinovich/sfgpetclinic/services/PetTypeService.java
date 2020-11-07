a4be6f7b273b39d479db93d80e150f396dfec959
ARTurchinovich <turchinovichar@gmail.com>
2020-11-07 17:31:26 +0300

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/ru/turchinovich/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package ru.turchinovich.sfgpetclinic.services;
+
+import ru.turchinovich.sfgpetclinic.model.PetType;
+
+import javax.xml.catalog.CatalogResolver;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
